111da2ce4493b04a788455355e19464fbcbea5dc
Linda Vu <lindavu@Lindas-MacBook-Pro.local>
2018-03-18 12:35:28 -0400

    HW3

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 # Normalize the data knowing the values are from 0 to 255
 def normalizeData(dataNorm):
-    return dataNorm / 255
+    return dataNorm / 255.0
 
 #If label = 6 then 0, if label = 8 then 1
 #Class 1: 6 and Class 2: 8
@@ -28,30 +28,17 @@ def gradientDescent(X, y, b):
     #reference https://github.com/michelucci/Logistic-Regression-Explained/blob/master/MNIST%20with%20Logistic%20Regression%20from%20scratch.ipynb
     predictions = prediction(X, b)
     sample = X.shape[0]
+    # error_cost = predictions - y
     error_cost = (predictions - y).transpose()
-    return -1.0 / sample * (numpy.dot(X.transpose(), error_cost))
-
-#Counts the number of true data
-# def trueCount(data):
-#     return sum(data)
-
-#Counts the number of false data
-# def falseCount(data):
-#     return (len(data) - trueCount(data))
+    # return  numpy.dot(X.transpose(), predictions - y)
+    return -1.0 / sample * numpy.dot(X.transpose(), error_cost)
 
 #sigmoid function
-def sigmoid(z):
-    return 1/(1.0 + numpy.exp(z))
+def sigmoid(value):
+    return 1/(1.0 + numpy.exp(-value))
 
-#Get a array of ones
-def onesMatrix(data):
-    return numpy.ones(data.shape[0], 1)
 
-#Get a array of zero
-def zeroMatrix(data):
-    return numpy.zeros(data.shape[1])
-
-def binaryPrediciton(predictionData):
+def binaryPrediction(predictionData):
     #array = predictionData
     for label in range(len(predictionData)):
         if predictionData[label] >= 0.5:
@@ -60,39 +47,20 @@ def binaryPrediciton(predictionData):
             predictionData[label] = 0
     return predictionData
 
-#Plot ROC Curve
-# def ROCCurve(TPR, FPR, predictionData, randomline = True):
-#     plt.figure()
-#     plt.xlabel("False Positive Rate", fontsize = 16)
-#     plt.ylabel("True Positive Rate", fontsize = 16)
-#     plt.title("ROV Curve", fontsize = 16)
-#
-#     TPR, FPR = CalcAccuracy(predictionData, normTestLabels)
-#
-#     plt.plot(FPR, TPR, normTestLabels=normTestLabels)
-#
-#     if randomline:
-#         x = [0.0, 1.0]
-#         plt.plot(x, x)
-#
-#     plt.xlim(0.0, 1.0)
-#     plt.ylim(0.0, 1.0)
 
-# def addZeroOne(rates):
-#     # Cost function
+# Cost function
 def cost(X, y, b):
     pr = prediction(X, b)
     C1 = -y * numpy.log(pr)
-    C2 = (1 - y) * numpy.log(1 - pr)
-    value = C1 - C2
-    value = value.sum() / len(y)
-    return value
+    C2 = -(1 - y) * numpy.log(1 - pr)
+    value = C1 + C2
+    return sum(value) / len(y)
 
 # likelihood function
-def likelihood(data, labels, bvalue):
-    scores = numpy.dot(data, bvalue)  # Takes the dot product of the data and bestimate
-    summation = numpy.sum(labels * scores - numpy.log(1 + numpy.exp(scores)))
-    return summation
+# def likelihood(data, labels, bvalue):
+#     scores = numpy.dot(data, bvalue)  # Takes the dot product of the data and bestimate
+#     summation = numpy.sum(labels * scores - numpy.log(1 + numpy.exp(scores)))
+#     return summation
 
 # This function makes the prediction using sigmond
 def prediction(X, b):
@@ -107,31 +75,26 @@ def accuracy(bestFitLine, testLabels):
     value = sum(bestFitLine == testLabels) / float(len(testLabels))
     return value
 
-# Accuracy = (TP+TN)/(TP+FP+FN+TN)
-def TPTNaccuracy(TP, TN, FP, FN):
-    return (TP + TN) / (TP + FP + FN + TN)
 
-def CalcAccuracy(predictionLabels, normTestLabels):
-    predictionLabels = (predictionLabels > .5)
+def CalcTPRandFPR(predictionLabels, normTestLabels):
+    predictionLabels = (predictionLabels > 0.5)
     trueCases = sum(normTestLabels)
     falseCases = len(normTestLabels) - trueCases
-    TP = 0
-    TN = 0
-    FP = 0
-    FN = 0
+    TP, TN, FP, FN = 0, 0, 0, 0
     for x in range(len(predictionLabels)):
-        if predictionLabels[x] == normTestLabels[x]:
+        if numpy.logical_and(predictionLabels[x], normTestLabels[x]):
             if predictionLabels[x] == 1:
                 TP += 1
             else:
                 TN += 1
         else:
-            if (predictionLabels[x] == 1 and normTestLabels[x] == 0):
-                FP += 1
-            else:
-                FN += 1
-    # print(trueCases)
-    # print(falseCases)
+            if (predictionLabels[x] != normTestLabels[x]):
+                if predictionLabels[x] == 1:
+                    FP += 1
+                else:
+                    FN += 1
+    print(trueCases)
+    print(falseCases)
     TPR = TP / float(trueCases)
     FPR = FP / float(falseCases)
     return TPR, FPR
@@ -142,6 +105,11 @@ data = numpy.genfromtxt('MNIST_CVHW3.csv', delimiter=',', dtype=int, skip_header
 
 kf = KFold(n_splits = 10)
 #print(kf)
+
+TPRArray = [] #y-axis
+FPRArray = [] #x-axis
+AccuracyArray = []
+print("Starting KFold")
 
 #Split the data with KFold
 for train, test in kf.split(data):
@@ -155,41 +123,58 @@ for train, test in kf.split(data):
     trainingData = numpy.array(Xytraining[:, 1: -1])
 
 
-
-
+    # print(trainingLabels)
     # Normalize Dataset and Labels
     normTestData = normalizeData(testingData)
     normTrainData = normalizeData(trainingData)
     normTestLabels = makeBinary(testingLabels)
     normTrainingLabels = makeBinary(trainingLabels)
 
+    # print(normTrainData[0])
+    # print(normTrainingLabels)
 
 
+    print("Starting Grad Decent")
     # Getting the b value (weights)
     bvalue = numpy.zeros(len(normTrainData[0]))  # weights
-    learningRate = 1e-4
-    iterator = 100
+    learningRate = 1e-2
+    iterator = 1000
+    cost_plot = []
 
     for x in range(0, iterator):
         bvalue = bvalue + learningRate * gradientDescent(normTrainData, normTrainingLabels, bvalue)
+        cost_plot.append(cost(normTrainData, normTrainingLabels, bvalue))
+
+
+    plt.plot(cost_plot)
+    plt.title('Cost Function')
+    plt.xlabel('Number of Iterations')
+    plt.ylabel('Learning Rate')
+    plt.show()
 
     # # Prediction with Gradient Descent
     pr = prediction(normTestData, bvalue)
+    print(binaryPrediction(pr))
 
-    #print(binaryPrediciton(pr))
 
     # plt.plot(cost(normTrainingLabels, normTestLabels, bvalue))
     # plt.show()
 
     ac = accuracy(pr, normTestLabels)
-    print(ac)
-    TPR, FPR = CalcAccuracy(pr, normTestLabels)
-print("TPR: {} \nFPR: {}".format(TPR, FPR))
+    AccuracyArray.append(ac)
+    TPR, FPR = CalcTPRandFPR(pr, normTestLabels)
+    TPRArray.append(TPR)
+    FPRArray.append(FPR)
 
-# plt.plot(TPR, FPR)
-# plt.xlim([0.0, 1.0])
-# plt.ylim([0.0, 1.0])
-# plt.xlabel('False Postive Rate')
-# plt.ylabel('True Positive Rate')
-# plt.show()
-# plt.show()
+print("TPR: {} \nFPR: {}".format(TPR, FPR))
+print("Accuracy: {}".format(ac))
+
+TPRArray += [0, 1] #y-axis adding 0 and 1 to the array
+FPRArray += [0, 1] #x-axis adding 0 and 1 to the array
+TPRArray.sort()
+FPRArray.sort()
+plt.plot(FPRArray, TPRArray)
+plt.title('ROC Curve')
+plt.ylabel('False Positive Rate')
+plt.xlabel('True Positive Rate')
+plt.show()
